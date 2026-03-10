ffbbf1fbebd7a2385d9676662e533668b241ad45
little_air <b08409045@gmail.com>
2026-03-10 11:39:23 +0800

    init project

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace line_bot_with_openai;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
